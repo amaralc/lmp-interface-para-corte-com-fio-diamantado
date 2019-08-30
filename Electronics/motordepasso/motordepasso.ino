4284725084340fbc1a874c962fe627f890460b12
alvsigor <alvs.igor@gmail.com>
2019-08-30 16:34:19 -0300

    CODE: Substituicao da versao 1 pela versao 2 do codigo de controle do motor de passo

--- Electronics/motordepasso/motordepasso.ino
+++ Electronics/motordepasso/motordepasso.ino
@@ -1,22 +1,22 @@
 #include <Stepper.h> //INCLUSÃO DE BIBLIOTECA
 #define potPin 0 // pino do potenciometro
+        
         const int stepsPerRevolution = 64; //NÚMERO DE PASSOS POR VOLTA
         int stepCount = 0;  
         int valPot = 0;
 
     Stepper myStepper(stepsPerRevolution, 8,10,9,11); //INICIALIZA O MOTOR UTILIZANDO OS PINOS DIGITAIS
 
-void setup(){
-  
-  }//end setup
+void setup(){ }//end setup
  
 void loop(){
  
          valPot = analogRead(potPin);
-         if (valPot < 470){
-            int motorSpeed = map(valPot,500,0,0,100);
+        /*1*/ if (valPot < 470){
+              
+                int motorSpeed = map(valPot,500,0,0,100);
          
-         if (motorSpeed > 0){
+        /*2*/ if (motorSpeed > 0){
           myStepper.setSpeed(motorSpeed);
           myStepper.step(stepsPerRevolution);
           
@@ -24,10 +24,10 @@ void loop(){
          }//end if1
            
            
-          if (valPot > 530) {
+         /*3*/ if (valPot > 530) {
            int motorSpeed = map(valPot,515,1024,0,100);
           
-          if (motorSpeed > 0){
+         /*4*/ if (motorSpeed > 0){
             myStepper.setSpeed(motorSpeed);
             myStepper.step(-stepsPerRevolution); 
             
